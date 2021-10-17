337faef479e6be376af333b97cfb47e99c067a5a
kvlin <kevlin129@hotmail.com>
2021-10-17 14:42:36 +1100

    Route for updating product by id

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,9 +15,60 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
-exports.getProducts = (req, res, next) => {
+// Get all products: /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
     res.status(200).json ({
         success: true,
-        message: 'This route will show all products in database.'
+        count: products.length,
+        products
     })
 }
+
+// Get single product: /api/v1/product/:id
+exports.getSingleProduct = async (req, res, next) => {
+    //find by id from req param
+    const product = await Product.findById(req.params.id)
+    
+    // if not found
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message:'Product not found'
+        })
+    }
+
+    res.status(202).json({
+        success: true,
+        product
+    })
+
+}
+
+// Update product: /api/v1/admin/product/:id
+exports.updateProduct = async (req, res, next) => {
+    //find by id from req param
+    let product = await Product.findById(req.params.id)
+    
+    // if not found
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message:'Product not found'
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+
+}
+
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct,updateProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
 
 router.route('/product/new').post(newProduct);
+
+router.route('/admin/product/:id').get(getSingleProduct);
+
+router.route('/admin/product/:id').put(updateProduct)
+
 
 module.exports = router;
